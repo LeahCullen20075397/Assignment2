5ccafe3bbecdf5a718e5f9dfbf3bef30dbb7370e
Leah Cullen <leahclcv@gmail.com>
2020-07-26 19:09:00 +0100

    kpop part 3

--- api/posts/index.js
+++ api/posts/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {posts} from './posts';
 import {comments} from './posts';
+import stubAPI from './stubAPI';
 
 const router = express.Router(); // eslint-disable-line
-router.get('/', (req, res) => {
+
+/*router.get('/', (req, res) => {
   res.send({posts: posts});
 });
 
@@ -19,6 +21,34 @@ router.post('/', (req, res) => {
 }else{
       res.status(400).send({message: "Unable to find Post in request. No Post Found in body"});
 }
+});*/
+
+// get all posts
+router.get('/', (req, res) => {
+  const posts = stubAPI.getAll();
+  res.send({posts: posts});
+});
+
+
+// Add a post
+router.post('/', (req, res) => {
+    const newPost = req.body;
+
+    if (newPost && stubAPI.add(newPost.event, newPost.poster, newPost.location, newPost.date, newPost.time, newPost.link)) {
+         return res.status(201).send({message: 'Posts Created'});
+    }
+    return res.status(400).send({message: 'Unable to find Post in request.'});
+});
+
+// get a post
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const post = stubAPI.getPost(id);
+
+       if (post) {
+               return res.status(200).send(post);
+              }
+              return res.status(404).send({message: `Unable to find Post ${id}`});
 });
 
 export default router;
